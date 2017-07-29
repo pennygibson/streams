8ac51d8494d4bf3be167ce2ef6242af9f5aaacca
pennygibson <cenneboulet@yahoo.ca>
2017-07-29 22:40:55 +0000

    modify to read the data

--- https_examples.js
+++ https_examples.js
@@ -8,9 +8,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log("I'm response handler callback!");
-  console.log('Response', response);
+
+  response.on('data', function(chunk){
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
